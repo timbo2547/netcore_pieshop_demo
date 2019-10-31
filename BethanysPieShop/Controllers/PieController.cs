b165622ee9820a5ccfaab50d04acf107a3cc777d
timbo2547 <timbo2547@gmail.com>
2019-10-30 22:58:50 -0500

    Toolbar/ Links
    toolbar
    links
    new action details

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -38,6 +38,14 @@ namespace BethanysPieShop.Controllers
             return View(piesListViewModel);
         }
 
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null)
+                return NotFound();
+            return View(pie);             
+        }
+
 
 
         //// GET: /<controller>/
